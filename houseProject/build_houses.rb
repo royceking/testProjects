581ec2e6f627725f7fb5763c0b5cffe9b94dc941
Royce King <roycewking@gmail.com>
2015-03-09 07:14:25 -0600

    thor gem

--- houseProject/build_houses.rb
+++ houseProject/build_houses.rb
@@ -1,84 +1,100 @@
 require "./house_obj.rb"
 require "awesome_print"
+require "thor"
  
-intitial_money = 20000
- 
-house1 = House.new(155000, 10000, 830, 0.04, 120, 30, 1)
+class Rental < Thor 
+	no_tasks do
 
-years = (1..30).to_a
- 
-# puts "Monthly Loan Payment = $#{house1.monthly_payment}"
-# puts "Monthly Insurance = $#{house1.monthly_insurance}"
-# puts "Monthly Taxes = $#{house1.monthly_taxes}"
-# puts "Monthly MIP = $#{house1.monthly_mip}"
-# puts "Monthly Cost = $#{house1.monthly_cost}"
-# puts "Proposed monly rent rate = $#{house1.rental_rate}"
-# puts "Total paid to lender = $#{house1.total_loan_amount}"
-# puts "Total interest paid to lender = $#{house1.total_loan_interest}"
-# puts "$#{house1.total_loan_paid(180)}"
-# puts "$#{house1.total_loan_owed(180)}"
+		def initialize(*args)
+			super
+			@homes = {}
+		end
 
-homes = {}
+		# years.each do |year|
+		# 	puts year.to_s
+		# 	ap house1.stats["year_#{year}"]
+		# end
 
-years.each do |year|
-	house_price = (150000 + rand(100000))
-	taxes = (house_price * 0.006).round(2)
-	insurance = 130
-	downpayment = 10000
-	term = 30
+		# @homes.each_value {|i| puts i.year}
 
-	homes["house#{year}_1"] = House.new(house_price, downpayment, taxes, 0.04, insurance, term, year)
-	#house = House.new(house_price, downpayment, taxes, 0.04, insurance, term)
+		def comma_numbers(number, delimiter = ',')
+		  number.to_s.reverse.gsub(%r{([0-9]{3}(?=([0-9])))}, "\\1#{delimiter}").reverse
+		end
 
-	#homes["house#{year}_2"] = House.new((150000 + rand(100000), downpayment, (house_price * 0.006).round(2), 0.04, insurance, term, year) if year < 10
-	# puts "******************************************"
-	# puts "House #{year}"
-	# puts
-	# puts "House Price = $#{house.price}"
-	# puts "House downpayment = $#{house.downpayment}"
-	# puts "Monthly Loan Payment = $#{house.monthly_payment}"
-	# puts "Monthly Insurance = $#{house.monthly_insurance}"
-	# puts "Monthly Taxes = $#{house.monthly_taxes}"
-	# puts "Monthly MIP = $#{house.monthly_mip}"
-	# puts "Monthly Cost = $#{house.monthly_cost}"
-	# puts "Proposed monly rent rate = $#{house.rental_rate}"
-	# puts "Total paid to lender = $#{house.total_loan_amount}"
-	# puts "Total interest paid to lender = $#{house.total_loan_interest}"
-	# puts "******************************************"
-	# puts
+		def get_year_stats(year)
+			equity = 0
+			profit = 0
+			debt = 0
+			count = 0
+			downpayment = 0
+			cash_flow = 0
+			@homes.each_value do |home|
+				if year >= home.year
+					index = year - home.year
+					downpayment = home.downpayment if year == home.year
+					equity = equity + home.stats.values[index][:principal]
+					debt = debt + home.stats.values[index][:balance]
+					profit = profit + home.stats.values[index][:profit]
+					# puts home.get_cash_flow(year).to_s
+					cash_flow = cash_flow + home.stats.values[index][:cash_flow]
+					count += 1
+
+				end
+			end
+			return {:equity => equity, :profit => (profit - downpayment), :debt => debt, :count => count, :cash_flow => cash_flow}
+		end
 end
 
-# years.each do |year|
-# 	puts year.to_s
-# 	ap house1.stats["year_#{year}"]
-# end
 
-# puts "Monthly Loan Payment = $#{house1.monthly_payment}"
-# puts "Monthly Insurance = $#{house1.monthly_insurance}"
-# puts "Monthly Taxes = $#{house1.monthly_taxes}"
-# puts "Monthly MIP = $#{house1.monthly_mip}"
-# puts "Monthly Cost = $#{house1.monthly_cost}"
-# puts "Proposed monly rent rate = $#{house1.rental_rate}"
-# puts "Total paid to lender = $#{house1.total_loan_amount}"
-# puts "Total interest paid to lender = $#{house1.total_loan_interest}"
+#-------------------------------------------------------------
+# 									Commandline Commands.
+#-------------------------------------------------------------
 
 
-years.each do |year|
-	profit = 0
-	principal = 0
-	debt = 0
-	count = 0
-	years.each do |year2|
-		if year2 <= year 
-			count += 1
-			principal = principal + homes["house#{year}_1"].stats["year_#{year}"][:principal]
-			debt = debt + homes["house#{year}_1"].stats["year_#{year}"][:balance]
-			profit = profit + homes["house#{year}_1"].stats["year_#{year}"][:profit]
+	desc "new", "New rental house."
+	def new (price, rate, insurance, term)
+		house1 = House.new(price.to_i, rate.to_f, insurance.to_i, term.to_i)
+		puts "Loan price = $#{house1.price}"
+		puts "Loan rate = $#{house1.interest_rate}"
+		puts "Downpayment = $#{house1.downpayment}"
+		puts "Monthly Loan Payment = $#{house1.monthly_payment.round}"
+		puts "Monthly Insurance = $#{house1.monthly_insurance}"
+		puts "Monthly Taxes = $#{house1.monthly_taxes}"
+		puts "Monthly PMI = $#{house1.monthly_pmi}"
+		puts "Monthly Cost = $#{house1.monthly_cost.round}"
+		puts "Proposed monly rent rate = $#{house1.rental_rate}"
+		puts "Total paid to lender = $#{house1.total_loan_amount.round}"
+		puts "Total interest paid to lender = $#{house1.total_loan_interest.round}"
+		# puts "$#{house1.total_loan_paid(180)}"
+		# puts "$#{house1.total_loan_owed(180)}"
+	end
+
+	desc "predict", "Random houses, predicts outcome and rental rate."
+	def predict
+
+		(1..30).each do |year|
+			house_price = (150000 + rand(100000))
+			insurance = 130
+			term = 30
+
+			@homes["house#{year}"] = House.new(house_price, 0.04, insurance, term)
+		end
+
+		(1..60).each do |year|
+				stats = self.get_year_stats(year)
+				profit = self.comma_numbers(stats[:profit].round)
+				equity = self.comma_numbers(stats[:equity].round)
+				debt = self.comma_numbers(stats[:debt].round)
+				cash_flow = self.comma_numbers(stats[:cash_flow].round)
+				count = stats[:count]
+
+			puts "In year #{year}; house count = #{count}; Equity = $#{equity}; Total debt = $#{debt}; Rental profit = $#{profit}; Cash Flow = $#{cash_flow}"
 		end
 	end
 
-	puts "In year #{year}, house count = #{count}, Equity = $#{principal.round(2)}, Total debt = $#{debt.round(2)}, Rental profit = $#{profit.round(2)}"
+
 end
 
 
+Rental.start(ARGV)
 
--- houseProject/house_obj.rb
+++ houseProject/house_obj.rb
@@ -1,17 +1,17 @@
 class House
-	attr_accessor :price, :downpayment, :taxes, :insurance, :interest_rate, :rental_rate, :monthly_payment, :loan_term, :loan_amount, :mip, :stats, :year
-	def initialize(price, downpayment, taxes, interest_rate, insurance, loan_term, year)
+	attr_accessor :price, :downpayment, :taxes, :insurance, :interest_rate, :rental_rate, :monthly_payment, :loan_term, :loan_amount, :pmi, :stats, :year
+	def initialize(price, interest_rate, insurance, loan_term)
 		@price = price
-		@downpayment = downpayment
+		@downpayment = price * 0.10
 		@loan_amount = price - downpayment
-		@taxes = taxes
+		@taxes = price * 0.006
 		@interest_rate = interest_rate
 		@insurance = insurance
 		@loan_term = loan_term
 		@monthly_payment = self.get_monthly_payment
 		@rental_rate = self.monthly_cost(200).round(-2)
 		@stats = self.build_stats
-		@year = year
+		@year = 0
 	end
  
 	def get_yearly_cost (extra_costs = 0)
@@ -29,14 +29,18 @@ class House
 		monthly_payment = @loan_amount * ((monthly_rate * term)/(term - 1))
 		return monthly_payment
 	end
+
+	def after_loan_profit
+		profit = @rental_rate - (self.monthly_taxes + self.monthly_insurance)
+	end
  
-	def get_mip
+	def get_pmi
 		value = @downpayment/@price
 		rate = 0.008 if value > 0.05
 		rate = 0.0085 if value < 0.05
 		rate = 0 if value > 0.20
-		mip = @loan_amount * rate
-		return mip.round(2)
+		pmi = @loan_amount * rate
+		return pmi.round(2)
 	end
  
 	def total_loan_amount
@@ -56,7 +60,7 @@ class House
 	end
  
 	def total_cost
-		total = self.total_loan_amount + self.total_taxes_paid + self.total_insurance_paid + self.total_mip_paid
+		total = self.total_loan_amount + self.total_taxes_paid + self.total_insurance_paid + self.total_pmi_paid
 	end
  
 	def total_taxes_paid
@@ -67,8 +71,8 @@ class House
 		total = @insurance * @loan_term
 	end
  
-	def total_mip_paid
-		total = @monthly_mip * 12 * @loan_term
+	def total_pmi_paid
+		total = @monthly_pmi * 12 * @loan_term
 	end
  
 	def monthly_taxes
@@ -79,22 +83,30 @@ class House
 		total = @insurance/12
 	end
  
-	def monthly_mip
-		total = (self.get_mip/12).round(2)
+	def monthly_pmi
+		total = (self.get_pmi/12).round(2)
 	end
  
 	def monthly_cost (extra_costs = 0)
-		total = @monthly_payment + self.monthly_mip + self.monthly_taxes + self.monthly_insurance + extra_costs 
+		total = @monthly_payment + self.monthly_pmi + self.monthly_taxes + self.monthly_insurance + extra_costs 
 	end
 
 	def build_stats
 		stats = {}
-		(@loan_term + 1).times do |year|
+		y = @year
+		(1..60).each do |year|
 			payments = year * 12
 			stats["year_#{year}"] = self.get_loan_stats(payments)
 		end
 		return stats
+	end
 
+	def get_cash_flow(year)
+		pmi = self.monthly_pmi
+		pmi = 0 if year > 5
+		cash_flow = (@rental_rate - (@monthly_payment + pmi + self.monthly_taxes + self.monthly_insurance))* 12
+		cash_flow = (@rental_rate - (self.monthly_taxes + self.monthly_insurance)) * 12 if year > 30
+		return cash_flow
 	end
 
 	def get_loan_stats (payments)
@@ -102,18 +114,22 @@ class House
 		interest = 0
 		balance = @loan_amount
 		profit = 0
-		(payments + 1).times do |p|
-			unless p == 0
-				payment_interest = (balance * @interest_rate)/12
-				payment_principal = @monthly_payment - payment_interest
+		(1..payments).each do |p|
+			pmi = self.monthly_pmi
+			pmi = 0 if p > 75
+			payment_interest = (balance * @interest_rate)/12
+			payment_principal = @monthly_payment - payment_interest
+			if p <= 360
 				principal = principal + payment_principal
 				interest += payment_interest
 				balance = balance - payment_principal
-				profit = profit + (@rental_rate - @monthly_payment)
-				# puts "Payment #{p} principal #{principal.round(2)}"
+				profit = profit + (@rental_rate - (@monthly_payment + pmi + self.monthly_taxes + self.monthly_insurance))
+			else
+				profit = profit + (@rental_rate - (self.monthly_taxes + self.monthly_insurance))
 			end
+
 		end
-		return {:balance => balance.round(2), :principal => principal.round(2), :interest => interest.round(2), :profit => profit}
+		return {:balance => balance.round(2), :principal => principal.round(2), :interest => interest.round(2), :profit => profit, :cash_flow => self.get_cash_flow(payments/12)}
 	end
 end
 
